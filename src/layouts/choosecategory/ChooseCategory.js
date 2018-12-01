c8d92512edc690ad0aa5c39a73ec8ed91466544b
trachanh1609 <Vu@Users-MacBook-Pro.local>
2018-12-01 21:44:13 +0200

    add key to iterator

--- src/layouts/choosecategory/ChooseCategory.js
+++ src/layouts/choosecategory/ChooseCategory.js
@@ -4,13 +4,13 @@ class ChooseCategory extends Component {
 
     state={
         categories: [
-            { name: 'Plastic', background: 'bg-info', icon: ''},
-            { name: 'Metal', background: 'bg-success', icon: ''},
-            { name: 'Battery', background: 'bg-danger', icon: ''},
-            { name: 'Glass', background: 'bg-warning', icon: ''},
-            { name: 'Mixed', background: 'bg-dark', icon: ''},
-            { name: 'Paper', background: 'bg-primary', icon: ''},
-            { name: 'E-waste', background: 'bg-info', icon: ''},
+            { name: 'Plastic', background: 'bg-info', id: '1'},
+            { name: 'Metal', background: 'bg-success', id: '2'},
+            { name: 'Battery', background: 'bg-danger', id: '3'},
+            { name: 'Glass', background: 'bg-warning', id: '4'},
+            { name: 'Mixed', background: 'bg-dark', id: '5'},
+            { name: 'Paper', background: 'bg-primary', id: '6'},
+            { name: 'E-waste', background: 'bg-info', id: '7'},
         ]
     };
 
@@ -30,7 +30,7 @@ class ChooseCategory extends Component {
           <div className="row">
             {this.state.categories.map(category=>{
                 return (
-                    <div className="col-6">
+                    <div className="col-6" key={category.id}>
                             <div className={"card mb-4 text-white text-center "+category.background}>
                                 <div className="card-body">
                                     <h3 className="card-text">{category.name}</h3>
